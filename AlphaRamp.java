8a46932e00d3f1bb18dc7cea4828885a74df2ba5
Kaveen Vignesh <tkkaveen77@gmail.com>
2024-08-20 14:01:27 +0530

    Alpha Ramp  pattern

---
+++ AlphaRamp.java
@@ -0,0 +1,14 @@
+public class AlphaRamp {
+   
+        public static void alphaRamp(int n) {
+           int ch=65;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<=i;j++){
+                    System.out.print((char)ch+" ");
+                }
+                System.out.println();
+                ch++;
+            }
+        }
+    }
+
